af0327679aab0b12e1f6b50f999b37d8d0a8aabb
alver joan perez cuellar <joan_todo@alvers-MBP.attlocal.net>
2021-12-27 21:48:22 -0800

    bucles

--- 13.bucles.py
+++ 13.bucles.py
@@ -2,9 +2,10 @@ def main():
     print("deseas continuar con el  programa?")
     palabra_si = input("escribe Y para continuar o N para parar: ")
 
-    while palabra_si == "y":
+    while palabra_si != "Y":
         palabra_si = input("escribe Y para continuar o N para parar: ")
-    print("fin del programa")
+        if palabra_si  == "N":
+            print("fin del programa")
 
 if __name__=="__main__":
     main()
